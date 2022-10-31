3b3cf96bab2c14dbf25d6480f67544a19f76fa05
william8622 <zhangqiang8622@gmail.com>
2022-10-31 22:54:42 +0900

    add some

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./cn_to_jp_word.json";
+import { Textarea, Button, Grid, Title, Container, Paper } from "@mantine/core";
 
-function App() {
+export default function App() {
+  const [cn, setCn] = useState("");
+  const [jp, setJp] = useState("");
+
+  const handleClick = (p) => {
+    const t = p === "cn" ? "jp" : "cn";
+    const text = p === "cn" ? cn : jp;
+    if (text) {
+      let r = [];
+      text.split("").forEach((w) => {
+        const res = data.filter((d) => d[p] === w)[0];
+        if (res) {
+          return r.push(res[t]);
+        } else {
+          return r.push(w);
+        }
+      });
+      p === "cn" && setJp(r.join(""));
+      p === "jp" && setCn(r.join(""));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title py={30} align='center'>
+        😉　中日汉字转换｜日中漢字変換　✌
+      </Title>
+      <Grid>
+        <Grid.Col md={6}>
+          <Paper shadow='sm' radius='xs' p='lg' md={6}>
+            <Title align='center' order={4}>
+              中文汉字→日语汉字
+            </Title>
+            <Textarea
+              placeholder='请输入中文汉字'
+              radius='xs'
+              autosize
+              minRows={10}
+              maxRows={10}
+              value={cn}
+              my={10}
+              onChange={(e) => {
+                setCn(e.target.value);
+                setJp("");
+              }}
+            />
+            <Button fullWidth onClick={() => handleClick("cn")}>
+              中→日
+            </Button>
+          </Paper>
+        </Grid.Col>
+        <Grid.Col md={6}>
+          <Paper shadow='sm' radius='xs' p='lg' md={6}>
+            <Title align='center' order={4}>
+              日本語漢字→中国語漢字
+            </Title>
+            <Textarea
+              placeholder='日本語漢字を入力してください'
+              radius='xs'
+              autosize
+              minRows={10}
+              maxRows={10}
+              my={10}
+              value={jp}
+              onChange={(e) => {
+                setJp(e.target.value);
+                setCn("");
+              }}
+            />
+            <Button
+              fullWidth
+              color='red'
+              radius='md'
+              onClick={() => handleClick("jp")}>
+              日→中
+            </Button>
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 }
-
-export default App;
